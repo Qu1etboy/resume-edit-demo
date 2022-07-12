4f9ee0a7d7c95c5b45d0d0bbf5ccbd313f9ab63d
Qu1etboy <nonzagreanthai@gmail.com>
2022-07-13 02:02:51 +0700

    fix(Footer, index) prettier error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import '../App.css';
 const Footer = () => {
   return (
     <div className="w-full text-center my-5 text-sm non-printable">
-      &copy; 2022 Qu1etboy. All Rights Reserved. 
+      &copy; 2022 Qu1etboy. All Rights Reserved.
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
